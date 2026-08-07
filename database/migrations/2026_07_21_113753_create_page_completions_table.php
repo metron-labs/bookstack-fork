5d4c378a8555f6f95b02f7b3cd8f8606463da0f6
Shubham Gupta <shubham.gupta@metronlabs.com>
2026-08-07 12:32:49 +0530

    minor fix for linting

--- database/migrations/2026_07_21_113753_create_page_completions_table.php
+++ database/migrations/2026_07_21_113753_create_page_completions_table.php
@@ -4,8 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-      /**
+return new class extends Migration
+{
+    /**
      * Run the migrations.
      *
      * @return void
@@ -21,8 +22,7 @@ return new class extends Migration {
             $table->unique(['user_id', 'page_id']);
         });
     }
-         
-    
+
     /**
      * Reverse the migrations.
      *
